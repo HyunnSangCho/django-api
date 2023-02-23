2f3da3bc8c49ede905177c2f76d2165e31088449
HyunnSangCho <zx251874@naver.com>
2023-02-24 01:39:17 +0900

    rm db

--- clone/views.py
+++ clone/views.py
@@ -4,14 +4,15 @@ from .serializers import CloneSerializer
 from rest_framework.response import Response
 
 
+
 class CloneView(APIView):
     def get(self, request):
         try:
-            page = request.query_params.get("page", 1)
+            page = request.query_params.get("page", 0)
             page = int(page)
         except ValueError:
-            page = 1
-        clone = Clone.objects.get(pk=page)
+            page = 0
+        clone = Clone.objects.get(pk=page+1)
         serializer = CloneSerializer(
             clone,
             context={"request": request},
