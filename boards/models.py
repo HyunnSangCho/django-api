1d5ab43c551ab8eb6c4cea5832f5183815e051be
HyunnSangCho <zx251874@naver.com>
2023-02-24 00:49:22 +0900

    edit model

--- boards/models.py
+++ boards/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Board(models.Model):
     subjects = models.ManyToManyField("subjects.Subject", related_name="boards")
-    description = models.CharField(max_length=350)
+    description = models.TextField()
     people = models.ForeignKey(
         "people.People",
         on_delete=models.CASCADE,
