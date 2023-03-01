fc1620e0699e9996c3e8506842d0066ea70fb15d
HyunnSangCho <zx251874@naver.com>
2023-03-02 03:48:00 +0900

    feat: edit views

--- app/pages/views.py
+++ app/pages/views.py
@@ -6,15 +6,22 @@ from rest_framework.response import Response
 
 
 class PageView(APIView):
+    def get_object(self, page):
+        try:
+            page = Page.objects.get(pk=page + 1)
+            return (page)
+        except Page.DoesNotExist:
+            self.get_object(page+1)
+
     def get(self, request):
         try:
             page = request.query_params.get("page", 0)
             page = int(page)
         except ValueError:
             page = 0
-        page = Page.objects.get(pk=page+1)
+        page_object = self.get_object(page)
         serializer = PageSerializer(
-            page,
+            page_object,
             context={"request": request},
         )
         return Response(serializer.data)
